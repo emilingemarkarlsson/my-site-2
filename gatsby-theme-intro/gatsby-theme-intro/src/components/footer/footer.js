f6d9cebdd783eee72f012a75d69012eddbe794ae
Emil I. Karlsson <emilingemarkarlsson@gmail.com>
2020-11-10 21:09:53 +0100

    update

--- gatsby-theme-intro/gatsby-theme-intro/src/components/footer/footer.js
+++ gatsby-theme-intro/gatsby-theme-intro/src/components/footer/footer.js
@@ -2,6 +2,7 @@ import { bool } from "prop-types"
 import React from "react"
 import { ProfileType } from "../../types"
 
+
 const Footer = ({ name, showThemeLogo = true }) => (
   <footer className="bg-front mt-16 pt-8 pb-16">
     <div className="md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 flex items-center">
--- gatsby-theme-intro/site/gatsby-config.js
+++ gatsby-theme-intro/site/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         basePath: "/",
         contentPath: "content/",
         showThemeLogo: true,
-        theme: "bright-green",
+        theme: "classic",
       },
     },
   ],
